fbab50df558aaee5c15727f10f5ce0aba57f1041
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-10-10 15:37:19 +0200

    Add license headers

--- simulation/perfect_codec_test.go
+++ simulation/perfect_codec_test.go
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
+// SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 package simulation
 
 import (
