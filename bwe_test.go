b95a7c593aec813a751778577a48b4442c00c05c
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-10-18 18:23:05 +0200

    Use a factory function for network config

--- bwe_test.go
+++ bwe_test.go
@@ -14,6 +14,7 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"testing/synctest"
 	"time"
@@ -23,6 +24,8 @@ import (
 )
 
 var logDir string
+
+type vnetFactory func(*testing.T) *virtualNetwork
 
 func TestMain(m *testing.M) {
 	logDir = os.Getenv("BWE_LOG_DIR")
@@ -63,111 +66,121 @@ func TestMain(m *testing.M) {
 }
 
 func TestBWE(t *testing.T) {
-	synctest.Test(t, func(t *testing.T) {
-		t.Helper()
+	networks := map[string]vnetFactory{
+		"constant_capacity": createVirtualNetwork(),
+	}
+	for name, vnf := range networks {
+		t.Run(name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				t.Helper()
 
-		logger, cleanup := testLogger(t)
-		defer cleanup()
+				logger, cleanup := testLogger(t)
+				defer cleanup()
 
-		onTrack := make(chan struct{})
-		connected := make(chan struct{})
-		done := make(chan struct{})
+				onTrack := make(chan struct{})
+				connected := make(chan struct{})
+				done := make(chan struct{})
 
-		network := createVirtualNetwork(t)
-		receiver, err := newPeer(
-			registerDefaultCodecs(),
-			setVNet(network.left, []string{"10.0.1.1"}),
-			registerTWCC(),
-			onRemoteTrack(func(track *webrtc.TrackRemote) {
-				close(onTrack)
-				go func() {
-					buf := make([]byte, 1500)
-					for {
-						select {
-						case <-done:
-							return
-						default:
-							_, _, err := track.Read(buf)
-							if errors.Is(err, io.EOF) {
-								return
+				network := vnf(t)
+
+				receiver, err := newPeer(
+					registerDefaultCodecs(),
+					setVNet(network.left, []string{"10.0.1.1"}),
+					registerTWCC(),
+					onRemoteTrack(func(track *webrtc.TrackRemote) {
+						close(onTrack)
+						go func() {
+							buf := make([]byte, 1500)
+							for {
+								select {
+								case <-done:
+									return
+								default:
+									_, _, err := track.Read(buf)
+									if errors.Is(err, io.EOF) {
+										return
+									}
+									assert.NoError(t, err)
+								}
 							}
-							assert.NoError(t, err)
-						}
-					}
+						}()
+					}),
+					registerPacketLogger(logger.With("vantage-point", "receiver")),
+					registerCCFB(),
+				)
+				assert.NoError(t, err)
+
+				err = receiver.addRemoteTrack()
+				assert.NoError(t, err)
+
+				var codec *perfectCodec
+				sender, err := newPeer(
+					registerDefaultCodecs(),
+					onConnected(func() { close(connected) }),
+					setVNet(network.right, []string{"10.0.2.1"}),
+					registerPacketLogger(logger.With("vantage-point", "sender")),
+					registerRTPFB(),
+					initGCC(func(rate int) {
+						logger.Info("setting codec target bitrate", "rate", rate)
+						codec.setTargetBitrate(rate)
+					}),
+				)
+				assert.NoError(t, err)
+
+				track, err := sender.addLocalTrack()
+				assert.NoError(t, err)
+
+				codec = newPerfectCodec(track, 1_000_000)
+				go func() {
+					<-connected
+					codec.start()
 				}()
-			}),
-			registerPacketLogger(logger.With("vantage-point", "receiver")),
-			registerCCFB(),
-		)
-		assert.NoError(t, err)
 
-		err = receiver.addRemoteTrack()
-		assert.NoError(t, err)
+				offer, err := sender.createOffer()
+				assert.NoError(t, err)
 
-		var codec *perfectCodec
-		sender, err := newPeer(
-			registerDefaultCodecs(),
-			onConnected(func() { close(connected) }),
-			setVNet(network.right, []string{"10.0.2.1"}),
-			registerPacketLogger(logger.With("vantage-point", "sender")),
-			registerRTPFB(),
-			initGCC(func(rate int) {
-				logger.Info("setting codec target bitrate", "rate", rate)
-				codec.setTargetBitrate(rate)
-			}),
-		)
-		assert.NoError(t, err)
+				err = receiver.setRemoteDescription(offer)
+				assert.NoError(t, err)
 
-		track, err := sender.addLocalTrack()
-		assert.NoError(t, err)
+				answer, err := receiver.createAnswer()
+				assert.NoError(t, err)
 
-		codec = newPerfectCodec(track, 1_000_000)
-		go func() {
-			<-connected
-			codec.start()
-		}()
+				err = sender.setRemoteDescription(answer)
+				assert.NoError(t, err)
 
-		offer, err := sender.createOffer()
-		assert.NoError(t, err)
+				synctest.Wait()
 
-		err = receiver.setRemoteDescription(offer)
-		assert.NoError(t, err)
+				select {
+				case <-onTrack:
+				case <-time.After(time.Second):
+					assert.Fail(t, "on track not called")
+				}
 
-		answer, err := receiver.createAnswer()
-		assert.NoError(t, err)
+				time.Sleep(100 * time.Second)
+				close(done)
 
-		err = sender.setRemoteDescription(answer)
-		assert.NoError(t, err)
+				err = codec.Close()
+				assert.NoError(t, err)
 
-		synctest.Wait()
-		select {
-		case <-onTrack:
-		case <-time.After(time.Second):
-			assert.Fail(t, "on track not called")
-		}
-		time.Sleep(100 * time.Second)
-		close(done)
+				err = sender.pc.Close()
+				assert.NoError(t, err)
 
-		err = codec.Close()
-		assert.NoError(t, err)
+				err = receiver.pc.Close()
+				assert.NoError(t, err)
 
-		err = sender.pc.Close()
-		assert.NoError(t, err)
+				err = network.Close()
+				assert.NoError(t, err)
 
-		err = receiver.pc.Close()
-		assert.NoError(t, err)
-
-		err = network.Close()
-		assert.NoError(t, err)
-
-		synctest.Wait()
-	})
+				synctest.Wait()
+			})
+		})
+	}
 }
 
 func testLogger(t *testing.T) (*slog.Logger, func()) {
 	t.Helper()
-
-	filename := filepath.Join(logDir, fmt.Sprintf("%s.jsonl", t.Name()))
+	name := strings.ReplaceAll(t.Name(), "/", "-")
+	filename := filepath.Join(logDir, fmt.Sprintf("%s.jsonl", name))
 	file, err := os.Create(filename)
 	if err != nil {
 		t.Fatalf("failed to create log file %q: %v", filename, err)
