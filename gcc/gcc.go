90cc797ece2c87339782b702656984bfd30d4577
Pion <59523206+pionbot@users.noreply.github.com>
2026-03-05 12:04:49 +0000

    Update CI configs to v0.12.1
    Update lint scripts and CI configs.

--- gcc/gcc.go
+++ gcc/gcc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package gcc implements a congestion controller based on
