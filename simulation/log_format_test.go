58f36365baf9d555c78802a0f5b291dec5348b79
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-04-27 18:06:12 +0200

    Add simulation tests

--- simulation/log_format_test.go
+++ simulation/log_format_test.go
@@ -1,35 +1,47 @@
 // SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
-//go:build !js
+//go:build !js && go1.25 && simulation
 
 package simulation
 
 import (
 	"fmt"
 	"log/slog"
-	"time"
 
 	"github.com/pion/interceptor"
 	"github.com/pion/rtcp"
 	"github.com/pion/rtp"
 )
 
+const (
+	maxSequenceNumberPlusOne = int64(65536)
+	breakpoint               = 32768 // half of max uint16
+)
+
 type packetLogger struct {
-	vantagePoint string
-	direction    string
+	logger    *slog.Logger
+	direction string
+	seq       *unwrapper
+}
+
+func newPacketLogger(logger *slog.Logger, direction string) *packetLogger {
+	return &packetLogger{
+		logger:    logger,
+		direction: direction,
+		seq:       &unwrapper{},
+	}
 }
 
 func (l *packetLogger) LogRTPPacket(header *rtp.Header, payload []byte, attributes interceptor.Attributes) {
-	ts := time.Now()
-	slog.Info(
+	u := l.seq.Unwrap(header.SequenceNumber)
+	l.logger.Info(
 		"rtp",
-		"vantage-point", l.vantagePoint,
 		"direction", l.direction,
-		"ts", ts,
 		"pt", header.PayloadType,
 		"ssrc", header.SSRC,
 		"sequence-number", header.SequenceNumber,
+		"unwrapped-sequence-number", u,
 		"rtp-timestamp", header.Timestamp,
 		"marker", header.Marker,
 		"payload-size", len(payload),
@@ -38,6 +50,48 @@ func (l *packetLogger) LogRTPPacket(header *rtp.Header, payload []byte, attribut
 
 func (l *packetLogger) LogRTCPPackets(pkts []rtcp.Packet, attributes interceptor.Attributes) {
 	for _, pkt := range pkts {
-		slog.Info("rtcp", "vantage-point", l.vantagePoint, "direction", l.direction, "type", fmt.Sprintf("%T", pkt))
+		l.logger.Info(
+			"rtcp",
+			"direction", l.direction,
+			"type", fmt.Sprintf("%T", pkt),
+		)
 	}
+}
+
+// Unwrapper stores an unwrapped sequence number.
+type unwrapper struct {
+	init          bool
+	lastUnwrapped int64
+}
+
+func isNewer(value, previous uint16) bool {
+	if value-previous == breakpoint {
+		return value > previous
+	}
+
+	return value != previous && (value-previous) < breakpoint
+}
+
+// Unwrap unwraps the next sequencenumber.
+func (u *unwrapper) Unwrap(i uint16) int64 {
+	if !u.init {
+		u.init = true
+		u.lastUnwrapped = int64(i)
+
+		return u.lastUnwrapped
+	}
+
+	lastWrapped := uint16(u.lastUnwrapped) //nolint:gosec // G115
+	delta := int64(i - lastWrapped)
+	if isNewer(i, lastWrapped) {
+		if delta < 0 {
+			delta += maxSequenceNumberPlusOne
+		}
+	} else if delta > 0 && u.lastUnwrapped+delta-maxSequenceNumberPlusOne >= 0 {
+		delta -= maxSequenceNumberPlusOne
+	}
+
+	u.lastUnwrapped += delta
+
+	return u.lastUnwrapped
 }
