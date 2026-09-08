3d3982054175332bcf6dd741f17dd901dd5b3d44
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-09-08 11:58:56 +0200

    Update transport and webrtc dependencies

--- simulation/vnet_test.go
+++ simulation/vnet_test.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"github.com/pion/logging"
-	"github.com/pion/transport/v3/vnet"
+	"github.com/pion/transport/v4/vnet"
 	"github.com/pion/webrtc/v4"
 	"github.com/stretchr/testify/assert"
 )
